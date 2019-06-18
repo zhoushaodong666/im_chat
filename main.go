14592ebfc3aba859bb326949005c5bccdca06d35
zhoushaodong666 <1138894663@qq.com>
2019-06-18 18:01:44 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main(){
+	//路由绑定
+	http.HandleFunc("/user/login", func(w http.ResponseWriter,r *http.Request) {
+		io.WriteString(w,"hello world!")
+		//数据库操作
+		//逻辑处理
+		//rest api json 返回
+		//1.获取前端传递的参数
+		//moblie passwd
+		//解析参数
+		r.ParseForm()
+		//如何获得参数
+		mobile:=r.PostForm.Get("mobile")
+		passwd:=r.PostForm.Get("passwd")
+
+		loginok:=false
+
+		if mobile=="13715277993"&&passwd=="123456" {
+			loginok = true
+		}
+
+		if loginok{
+			//成功
+			str:=`{"code":0,"data":{"id":1,"token":"test"}}`
+			//设置header 为JSON 默认的 text/html 所以特别指定返回个格式
+			//applicaltion/json
+			
+
+		}else{
+			//失败
+		}
+		//如何返回json
+
+	})
+
+	//启动web服务器
+	http.ListenAndServe(":8080",nil)
+}
