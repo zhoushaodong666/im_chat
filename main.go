0033bfea041064282690d103d623660d53e20ae1
zhoushaodong666 <1138894663@qq.com>
2019-06-18 18:04:15 +0800

    11

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"net/http"
 )
 
-func main(){
+func main() {
 	//路由绑定
-	http.HandleFunc("/user/login", func(w http.ResponseWriter,r *http.Request) {
-		io.WriteString(w,"hello world!")
+	http.HandleFunc("/user/login", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "hello world!")
 		//数据库操作
 		//逻辑处理
 		//rest api json 返回
@@ -17,29 +17,33 @@ func main(){
 		//解析参数
 		r.ParseForm()
 		//如何获得参数
-		mobile:=r.PostForm.Get("mobile")
-		passwd:=r.PostForm.Get("passwd")
+		mobile := r.PostForm.Get("mobile")
+		passwd := r.PostForm.Get("passwd")
 
-		loginok:=false
+		loginok := false
 
-		if mobile=="13715277993"&&passwd=="123456" {
+		if mobile == "13715277993" && passwd == "123456" {
 			loginok = true
 		}
 
-		if loginok{
+		if loginok {
 			//成功
-			str:=`{"code":0,"data":{"id":1,"token":"test"}}`
+			str := `{"code":0,"data":{"id":1,"token":"test"}}`
 			//设置header 为JSON 默认的 text/html 所以特别指定返回个格式
 			//applicaltion/json
-			
-
-		}else{
+			w.Header().Set("Content-Type", "applicaltion/json")
+			//设置状态码200
+			w.WriteHeader(http.StatusOK)
+			//写入ResponseWriter
+			w.Write([]byte(str))
+		} else {
 			//失败
+
 		}
 		//如何返回json
 
 	})
 
 	//启动web服务器
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080", nil)
 }
