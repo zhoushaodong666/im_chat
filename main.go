edb71ab0171881ac6f1a1c64c5fce0bb719259d4
zhoushaodong666 <1138894663@qq.com>
2019-06-19 09:58:56 +0800

    优化结构

--- main.go
+++ main.go
@@ -1,48 +1,14 @@
 package main
 
 import (
-	"io"
+	"im_chat/handle"
 	"net/http"
 )
 
 func main() {
 	//路由绑定
-	http.HandleFunc("/user/login", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "hello world!")
-		//数据库操作
-		//逻辑处理
-		//rest api json 返回
-		//1.获取前端传递的参数
-		//moblie passwd
-		//解析参数
-		r.ParseForm()
-		//如何获得参数
-		mobile := r.PostForm.Get("mobile")
-		passwd := r.PostForm.Get("passwd")
-
-		loginok := false
-
-		if mobile == "13715277993" && passwd == "123456" {
-			loginok = true
-		}
-		//成功
-		str := `{"code":0,"data":{"id":1,"token":"test"}}`
-		if !loginok {
-			//失败
-			str = `{"code":-1,"msg":"密码不正确"}`
-
-		}
-
-		//设置header 为JSON 默认的 text/html 所以特别指定返回个格式
-		//application/json
-		w.Header().Set("Content-Type", "application/json")
-
-		//设置状态码200
-		w.WriteHeader(http.StatusOK)
-		//写入ResponseWriter
-		w.Write([]byte(str))
-
-	})
+	//用户登录
+	http.HandleFunc("/user/login", handle.UserLogin)
 
 	//启动web服务器
 	http.ListenAndServe(":8080", nil)
